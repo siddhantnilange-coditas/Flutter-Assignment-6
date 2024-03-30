13f97930840dd8d58194cb66372dbb286ddd91e4
Siddhant Nilange <siddhant.nilange@coditas.com>
2024-03-30 18:14:35 +0530

    Provider

--- lib/screens/edit_data.dart
+++ lib/screens/edit_data.dart
@@ -52,7 +52,7 @@ class EditUsersScreen extends StatelessWidget {
                     ),
                   ),
                 ),
-                SizedBox(
+                const SizedBox(
                   height: 20,
                 ),
                 Container(
@@ -84,7 +84,7 @@ class EditUsersScreen extends StatelessWidget {
                     },
                   ),
                 ),
-                Container(
+                SizedBox(
                   height: 65,
                   width: MediaQuery.of(context).size.width * 0.9,
                   child: TextFormField(
@@ -110,10 +110,10 @@ class EditUsersScreen extends StatelessWidget {
                     },
                   ),
                 ),
-                SizedBox(
+                const SizedBox(
                   height: 20,
                 ),
-                Container(
+                SizedBox(
                   height: 65,
                   width: MediaQuery.of(context).size.width * 0.9,
                   child: TextFormField(
@@ -142,7 +142,7 @@ class EditUsersScreen extends StatelessWidget {
                     },
                   ),
                 ),
-                SizedBox(
+                const SizedBox(
                   height: 20,
                 ),
                 Container(
@@ -242,282 +242,3 @@ class EditUsersScreen extends StatelessWidget {
     );
   }
 }
-
-
-// Widget textFormFieldTemplate(String intialString, String labelOfFormField()){
-//   return Container(
-//                   height: 65,
-//                   width: MediaQuery.of(context).size.width * 0.9,
-//                   child: TextFormField(
-//                     controller: nameController,
-//                     maxLength: 15,
-//                     decoration: const InputDecoration(
-//                       labelText: 'Name',
-//                       labelStyle: TextStyle(color: Colors.blue),
-//                       hintText: 'Enter your name',
-//                       hintStyle: TextStyle(color: Colors.grey),
-//                       border: OutlineInputBorder(
-//                           borderRadius: BorderRadius.all(Radius.circular(12))),
-//                       focusedBorder: OutlineInputBorder(
-//                         borderRadius: BorderRadius.all(Radius.circular(18)),
-//                         borderSide: BorderSide(color: Colors.blue),
-//                       ),
-//                     ),
-//                     validator: (value) {
-//                       if(value!.isEmpty ||  value == null){
-//                         return 'Please enter your name';
-//                       }
-//                       else{
-//                         return null;
-//                       }
-//                     },
-//                   ),
-//                 );
-// }
-
-
-// import 'package:flutter/material.dart';
-// import 'package:magical_change/models/user_info.dart';
-// import 'package:magical_change/screens/start_screen.dart';
-// import 'package:provider/provider.dart';
-
-// class EditUsersScreen extends StatefulWidget {
-//   const EditUsersScreen(
-//       {required this.user,
-//       required this.title,
-//       required this.index,
-//       super.key});
-//   final User user;
-//   final int index;
-//   final String title;
-
-//   @override
-//   State<StatefulWidget> createState() {
-//     return EditUsersScreenState();
-//   }
-// }
-
-// class EditUsersScreenState extends State<EditUsersScreen> {
-//   late TextEditingController nameController;
-//   late TextEditingController emailController;
-//   late TextEditingController phoneNumberController;
-//   late TextEditingController addressController;
-
-//   @override
-//   void initState() {
-//     super.initState();
-//     nameController = TextEditingController(text: widget.user.name);
-//     emailController = TextEditingController(text: widget.user.email);
-//     phoneNumberController =
-//         TextEditingController(text: widget.user.phoneNumber);
-//     addressController = TextEditingController(text: widget.user.address);
-//   }
-
-//   @override
-//   void dispose() {
-//     nameController.dispose();
-//     emailController.dispose();
-//     phoneNumberController.dispose();
-//     addressController.dispose();
-//     super.dispose();
-//   }
-
-//   // final myController = TextEditingController();
-
-//   // @override
-//   // void dispose() {
-//   //   myController.dispose();
-//   //   super.dispose();
-//   // }
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         backgroundColor: const Color.fromARGB(255, 233, 159, 63),
-//         title: Text(widget.title),
-//       ),
-//       body: SingleChildScrollView(
-//         child: Padding(
-//           padding: const EdgeInsets.all(10.0),
-//           child: Column(
-//             // mainAxisAlignment: MainAxisAlignment.spaceBetween,
-//             children: [
-//               Container(
-//                 height: 140,
-//                 width: 140,
-//                 decoration: const BoxDecoration(
-//                   color: Color.fromARGB(255, 225, 177, 177),
-//                   shape: BoxShape.circle,
-//                   image: DecorationImage(
-//                     image: AssetImage('assets/images/men.png'),
-//                     fit: BoxFit.fill,
-//                   ),
-//                 ),
-//               ),
-//               SizedBox(
-//                 height: 20,
-//               ),
-//               Container(
-//                 height: 65,
-//                 width: MediaQuery.of(context).size.width * 0.9,
-//                 child: TextFormField(
-//                   controller: nameController,
-//                   maxLength: 15,
-//                   decoration: const InputDecoration(
-//                     labelText: 'Name',
-//                     labelStyle: TextStyle(color: Colors.blue),
-//                     hintText: 'Enter your name',
-//                     hintStyle: TextStyle(color: Colors.grey),
-//                     border: OutlineInputBorder(
-//                         borderRadius: BorderRadius.all(Radius.circular(12))),
-//                     focusedBorder: OutlineInputBorder(
-//                       borderRadius: BorderRadius.all(Radius.circular(18)),
-//                       borderSide: BorderSide(color: Colors.blue),
-//                     ),
-//                   ),
-//                 ),
-//               ),
-//               SizedBox(
-//                 height: 20,
-//               ),
-//               Container(
-//                 height: 65,
-//                 width: MediaQuery.of(context).size.width * 0.9,
-//                 child: TextFormField(
-//                   controller: emailController,
-//                   maxLength: 20,
-//                   decoration: const InputDecoration(
-//                     labelText: 'Email',
-//                     labelStyle: TextStyle(color: Colors.blue),
-//                     hintText: 'Enter your email',
-//                     hintStyle: TextStyle(color: Colors.grey),
-//                     border: OutlineInputBorder(
-//                         borderRadius: BorderRadius.all(Radius.circular(12))),
-//                     focusedBorder: OutlineInputBorder(
-//                       borderRadius: BorderRadius.all(Radius.circular(18)),
-//                       borderSide: BorderSide(color: Colors.blue),
-//                     ),
-//                   ),
-//                   validator: (value) {
-//                     if (value == null || value.isEmpty) {
-//                       return 'Please enter some text';
-//                     }
-//                     return null;
-//                   },
-//                 ),
-//               ),
-//               SizedBox(
-//                 height: 20,
-//               ),
-//               Container(
-//                 height: 65,
-//                 width: MediaQuery.of(context).size.width * 0.9,
-//                 child: TextFormField(
-//                   controller: phoneNumberController,
-//                   maxLength: 12,
-//                   decoration: const InputDecoration(
-//                     labelText: 'Phone Number',
-//                     labelStyle: TextStyle(color: Colors.blue),
-//                     // prefixText: '91',
-//                     hintText: 'Enter your phone number',
-//                     hintStyle: TextStyle(color: Colors.grey),
-//                     border: OutlineInputBorder(
-//                         borderRadius: BorderRadius.all(Radius.circular(12))),
-//                     focusedBorder: OutlineInputBorder(
-//                       borderRadius: BorderRadius.all(Radius.circular(18)),
-//                       borderSide: BorderSide(color: Colors.blue),
-//                     ),
-//                   ),
-//                 ),
-//               ),
-//               SizedBox(
-//                 height: 20,
-//               ),
-//               Container(
-//                 height: 65,
-//                 width: MediaQuery.of(context).size.width * 0.9,
-//                 child: TextFormField(
-//                   controller: addressController,
-//                   maxLength: 25,
-//                   decoration: const InputDecoration(
-//                     labelText: 'Address',
-//                     labelStyle: TextStyle(color: Colors.blue),
-//                     hintText: 'Enter your address',
-//                     hintStyle: TextStyle(color: Colors.grey),
-//                     border: OutlineInputBorder(
-//                         borderRadius: BorderRadius.all(Radius.circular(12))),
-//                     focusedBorder: OutlineInputBorder(
-//                       borderRadius: BorderRadius.all(Radius.circular(18)),
-//                       borderSide: BorderSide(color: Colors.blue),
-//                     ),
-//                   ),
-//                 ),
-//               ),
-//               const SizedBox(
-//                 height: 50,
-//               ),
-//               Row(
-//                 mainAxisAlignment: MainAxisAlignment.center,
-//                 children: [
-//                   ElevatedButton(
-//                     onPressed: () {
-//                       Navigator.pop(context);
-//                     },
-//                     style: ElevatedButton.styleFrom(
-//                         elevation: 4,
-//                         backgroundColor: const Color.fromARGB(255, 229, 66, 66),
-//                         padding: const EdgeInsets.symmetric(
-//                             horizontal: 30, vertical: 10),
-//                         textStyle: const TextStyle(
-//                             fontSize: 20, fontWeight: FontWeight.bold)),
-//                     child: const Text(
-//                       'Cancel',
-//                       style: TextStyle(color: Colors.black),
-//                     ),
-//                   ),
-//                   const SizedBox(
-//                     width: 40,
-//                   ),
-//                   ElevatedButton(
-//                     onPressed: () {
-//                       print('in submit');
-
-//                       UserProvider userProvider =
-//                           Provider.of<UserProvider>(context, listen: false);
-
-//                       userProvider.updateUser(
-//                         User(
-//                           name: nameController.text,
-//                           email: emailController.text,
-//                           phoneNumber: phoneNumberController.text,
-//                           address: addressController.text,
-//                           avatar: widget.user.avatar,
-//                         ),
-//                         widget.index,
-//                       );
-//                       Navigator.pop(context);
-//                     },
-//                     style: ElevatedButton.styleFrom(
-//                         elevation: 4,
-//                         backgroundColor:
-//                             const Color.fromARGB(255, 106, 219, 86),
-//                         padding: const EdgeInsets.symmetric(
-//                             horizontal: 30, vertical: 10),
-//                         textStyle: const TextStyle(
-//                             fontSize: 20, fontWeight: FontWeight.bold)),
-//                     child: const Text(
-//                       'Submit',
-//                       style: TextStyle(color: Colors.black),
-//                     ),
-//                   ),
-//                 ],
-//               )
-//             ],
-//           ),
-//         ),
-//       ),
-//     );
-//   }
-// }
-
--- lib/user_listview.dart
+++ lib/user_listview.dart
@@ -13,32 +13,11 @@ class UserListView extends StatelessWidget {
       itemCount: userInfo.length,
       itemBuilder: (context, index) => UserItem(
         index: index,
-        // updateUser: updateUser,
-        // userInfo: widget.userInfo[index],
+
       ),
     );
   }
 
-  // @override
-  // State<UserListView> createState() => _UserListViewState();
 }
 
-// class _UserListViewState extends State<UserListView> {Z
-  // void updateUser(int index, User updatedUser) {
-  //   setState(() {
-  //     widget.userInfo[index] = updatedUser;
-  //   });
-  // }
 
-//   @override
-//   Widget build(BuildContext context) {
-//     return ListView.builder(
-//       itemCount: widget.userInfo.length,
-//       itemBuilder: (context, index) => UserItem(
-//         index: index,
-//         updateUser: updateUser,
-//         userInfo: widget.userInfo[index],
-//       ),
-//     );
-//   }
-// }
