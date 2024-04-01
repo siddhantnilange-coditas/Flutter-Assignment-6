3969ea3f34f283ecb6a127a2b7dc28e98f950aac
Siddhant Nilange <siddhant.nilange@coditas.com>
2024-04-01 20:17:46 +0530

    Riverpod

--- lib/riverpod/user_riverpod.dart
+++ lib/riverpod/user_riverpod.dart
@@ -67,130 +67,46 @@ class UserProvider extends StateNotifier<List<User>> {
   void addUser(newUser) {
     state.add(newUser);
   }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  
-  // void updateUser(User newUser, int index) {
-  //   print("In update user");
-  //   state[index] = newUser;
-  //       print("out update user");
-
-  // }
-// void printUserList() {
-//     for(int i=0; i<6; i++){
-//       print(state[i]); 
-//   }
-//     }
-  // void removeUser(int index) {
-  //   state.removeAt(index);
-  // // }
-  // }
 }
 
 
-// class UserProvider with ChangeNotifier {
-//   final List<User> _users = [
-//     User(
-//       address: 'Pune',
-//       name: 'Sumit Ambhore',
-//       email: 'sumit@sumit.com',
-//       phoneNumber: '4565465656',
-//       avatar: 'assets/images/person.jpeg',
-//     ),
-//     User(
-//       address: 'Parbhani',
-//       name: 'Siddhant',
-//       email: 'Sidd@sidd.com',
-//       phoneNumber: '983459998',
-//       avatar: 'assets/images/person.jpeg',
-//     ),
-//     User(
-//       address: 'Jalgaon',
-//       name: 'Gaurav',
-//       email: 'Gaurav@gaurav.com',
-//       phoneNumber: '1234567890',
-//       avatar: 'assets/images/person.jpeg',
-//     ),
-//     User(
-//       address: 'Pune',
-//       name: 'Mayur',
-//       email: 'Mayur@mayur.com',
-//       phoneNumber: '43454546544',
-//       avatar: 'assets/images/person.jpeg',
-//     ),
-//     User(
-//       address: 'Nagpur',
-//       name: 'Yash',
-//       email: 'Yash@yash.com',
-//       phoneNumber: '4544354543543',
-//       avatar: 'assets/images/person.jpeg',
-//     ),
-//     User(
-//       address: 'Pune',
-//       name: 'Aniket',
-//       email: 'aniket@aniket.com',
-//       phoneNumber: '3323434343',
-//       avatar: 'assets/images/person.jpeg',
-//     ),
-//   ];
-//   List<User> get users => _users;
 
 
-//   void updateUser(User newUser, int index) {
-//     if(index>=0 && index<_users.length){
-//           _users[index] = newUser;
-//     }
-//     else{
-//       addUser(newUser);
-//     }
 
-//     notifyListeners();
-//   }
 
-//   void addUser(newUser) {
-//     _users.add(newUser);
-//     notifyListeners();
-//   }
-// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- lib/user_item.dart
+++ lib/user_item.dart
@@ -13,7 +13,6 @@ class UserItem extends StatelessWidget {
   final int index;
   @override
   Widget build(BuildContext context) {
-    // User userInfo = Provider.of<UserProvider>(context).users[index];
     return Consumer(
       builder: (context, ref, child) {
         final userDetail = ref.watch(userRiverpod);
@@ -147,82 +146,3 @@ class UserItem extends StatelessWidget {
 
 
 
-
-
-
-
-
-
-// import 'package:state_demo_1/models/User.dart';
-// import 'package:state_demo_1/riverpod/user_riverpod.dart';
-// import 'package:state_demo_1/update_user.dart';
-
-// class UserItem extends StatelessWidget {
-//   const UserItem(this.userIndex,  
-// {super.key});
-
-//   final userIndex;
-//   // final int index;
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Consumer(
-//       builder: (context, ref, child) {
-//         final userDetail = ref.watch(userRiverpod);
-//         return Padding(
-//             padding: const EdgeInsets.all(4),
-//             child: GestureDetector(
-//               onTap: () {
-//                 Navigator.push(
-//                   context,
-//                   MaterialPageRoute(
-//                     builder: (context) => EditUsersScreen(
-//                       user: userDetail[userIndex],
-//                       index: userIndex,
-//                       title: 'Edit data',
-//                       ),
-//                   ),
-//                 );
-//               },
-//               child: Card(
-//                 elevation: 2,
-//                 child: Padding(
-//                   padding:
-//                       const EdgeInsets.symmetric(horizontal: 10, vertical: 2),
-//                   child: Row(
-//                     children: [
-//                       Image.asset(
-//                         "assets/images/men.png",
-//                         width: 100,
-//                         height: 100,
-//                       ),
-//                       Expanded(
-//                         child: Padding(
-//                           padding: const EdgeInsets.symmetric(horizontal: 10),
-//                           child: Column(
-//                             crossAxisAlignment: CrossAxisAlignment.start,
-//                             children: [
-//                               Text(
-//                                 userDetail[userIndex].name,
-//                                 style: const TextStyle(
-//                                     fontWeight: FontWeight.bold, fontSize: 16),
-//                               ),
-//                               Text(
-//                                   "Email : ${userDetail[userIndex].email}"),
-//                               Text(
-//                                   "Phone No. :  ${userDetail[userIndex].phoneNumber}"),
-//                               Text(
-//                                   "Address :  ${userDetail[userIndex].address}"),
-//                             ],
-//                           ),
-//                         ),
-//                       )
-//                     ],
-//                   ),
-//                 ),
-//               ),
-//             ));
-//       },
-//     );
-//   }
-// }
